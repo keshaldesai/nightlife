1ac360c9914eb1e0855fe99ff7e3559d1c8fbbc5
Keshal Desai <novalearngood@gmail.com>
2017-06-06 14:12:20 -0400

    CRA initialized

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -1,3 +1,18 @@
+const request = require('request');
+const API_KEY = require('../config/placesConfig.json').API_KEY;
+const googleMapsClient = require('@google/maps').createClient({
+	key: API_KEY
+});
+
 module.exports = function (app) {
 	//API route handlers
+	app.get('/api/search', function (req, res) {
+		googleMapsClient.geocode({
+			address: '1600 Amphitheatre Parkway, Mountain View, CA'
+		}, function (err, response) {
+			if (!err) {
+				console.log(response.json.results);
+			}
+		});
+	});
 }
